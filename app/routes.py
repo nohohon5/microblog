df6899427dc82c2566d7548efabf97a10ac1a663
nohohon@yeah.net <nohohon@yeah.net>
2020-03-06 17:09:08 +0800

    登录页面添加

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template,flash,redirect,url_for
+from flask import render_template,flash,redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -18,12 +18,12 @@ def index():
     ]
     return render_template('index.html',title='Home',user=user,posts=posts)
 
-@app.route('/login',methods=['GRT','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash("login requested for user {},remember_me={}".format(
             form.username.data,form.remember_me.data
         ))
-        return redirect(url_for('/index'))
-    return render_template(url_for('login.html'),title="sign in",form=form)
+        return redirect('index')
+    return render_template('login.html',title="sign in",form=form)
--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get(("SCRET_KEY")) or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    SQLALCHEMY_COMMIT_TEARDOWN = True
+
