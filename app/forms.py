675bb2459c5e9ebfddb3eee622976092c5cec5f6
nohohon@yeah.net <nohohon@yeah.net>
2020-03-09 22:21:25 +0800

    个人信息页添加

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,TextAreaField
 from app.models import User
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
@@ -26,3 +26,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username",validators=[DataRequired()])
+    about_me = TextAreaField('About me',validators=[Length(min=0,max=140)])
+    submit = SubmitField("Submit")
